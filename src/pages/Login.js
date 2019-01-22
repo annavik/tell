015a9ec855d5f46bd3e3de68317173daf6b7fb91
Anna Viklund <anna.viklund@hotmail.com>
2019-01-21 22:35:27 -0800

    Add some assets and render a background image

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,22 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, StatusBar, StyleSheet } from 'react-native'
+
+const bg = require('../../assets/images/bg_login.jpg')
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
   },
 })
 
 export class Login extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Login</Text>
-      </View>
+      <ImageBackground source={bg} style={styles.container}>
+        <StatusBar barStyle="light-content" />
+      </ImageBackground>
     )
   }
 }
